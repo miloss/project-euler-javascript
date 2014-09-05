181100f025819cbb60c5c9e7e90ba7374cae1197
Milos Popovic <zaghy1@yahoo.co.uk>
2014-09-05 13:36:01 +0200

    solving problem 30

---
+++ 030.js
@@ -0,0 +1,34 @@
+
+// Surprisingly there are only three numbers that can be written as the sum of 
+// fourth powers of their digits:
+
+//    1634 = 1^4 + 6^4 + 3^4 + 4^4
+//    8208 = 8^4 + 2^4 + 0^4 + 8^4
+//    9474 = 9^4 + 4^4 + 7^4 + 4^4
+
+// As 1 = 1^4 is not a sum it is not included.
+
+// The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+
+// Find the sum of all the numbers that can be written as the sum of fifth 
+// powers of their digits.
+
+function isFifthPowerDigitsNumber(n) {
+	var sum = 0;
+	var number = n;
+	while (number) {
+		sum += Math.pow(number % 10, 5);
+		number = Math.floor(number/10);
+	}
+	return sum === n;
+}
+
+function fifthPowerDigitNumbersSum(max) {
+	var sum = 0;
+	while (max-- > 10) {
+		if (isFifthPowerDigitsNumber(max)) sum += max;
+	}
+	return sum;
+}
+
+console.log( fifthPowerDigitNumbersSum(1000000) );
