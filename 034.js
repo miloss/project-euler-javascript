468ddb9a898183c314d241e6ee8cb88768d34e6c
Milos Popovic <zaghy1@yahoo.co.uk>
2014-09-08 13:58:23 +0200

    solving problem 34

--- 034.js
+++ 034.js
@@ -5,3 +5,26 @@
 // their digits.
 
 // Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+
+function fac(digit) {
+	var facts = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
+	return facts[parseInt(digit, 10)];
+}
+
+function findCuriousNumbersSum(max) {
+	var total = 0;
+	var sum, digits, i;
+	for (i = 10; i <= max; i++) {
+		sum = 0;
+		digits = (i+'').split('');
+		while (digits.length) {
+			sum += fac(digits.shift());
+		}
+		if (sum === i) {
+			total += i;
+		}
+	}
+	return total;
+}
+
+console.log( findCuriousNumbersSum(1000000) );
