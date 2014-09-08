2bb69c6fc0b97f099a5f2f8d10658e7d0f194088
Milos Popovic <zaghy1@yahoo.co.uk>
2014-09-08 14:36:25 +0200

    solving problem 32

--- 032.js
+++ 032.js
@@ -12,3 +12,50 @@
 // HINT: Some products can be obtained in more than one way so be sure to only 
 // include it once in your sum.
 
+
+function permutate(array, callback) {
+	function p(array, index, callback) {
+		function swap(a, i1, i2) {
+			var t = a[i1];
+			a[i1] = a[i2];
+			a[i2] = t;
+		}
+		if (index == array.length - 1) {
+			callback(array);
+			return 1;
+		} else {
+			var count = p(array, index + 1, callback);
+			for (var i = index + 1; i < array.length; i++) {
+				swap(array, i, index);
+				count += p(array, index + 1, callback);
+				swap(array, i, index);
+			}
+			return count;
+		}
+	}
+
+	return p(array, 0, callback);
+}
+
+function findUnusualProducts() {
+	var products = [];
+	permutate([1,2,3,4,5,6,7,8,9], function(arr){
+		var a, b, c;
+		var i, j;
+		for (i = 0; i < 7; i++) {
+			for (j = i+1; j < 8; j++) {
+				a = parseInt(arr.slice(0, i+1).join(''), 10);
+				b = parseInt(arr.slice(i+1, j+1).join(''), 10);
+				c = parseInt(arr.slice(j+1).join(''), 10);
+				if (a*b === c) {
+					if (products.indexOf(c) === -1) {
+						products.push(c);
+					}
+				}
+			}
+		}
+	});
+	return products;
+}
+
+console.log( findUnusualProducts() );
