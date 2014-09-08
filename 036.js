0d0a50611a57ae725b50c869ce954d6e50c86e54
Milos Popovic <zaghy1@yahoo.co.uk>
2014-09-08 15:04:21 +0200

    solving problem 36

--- 036.js
+++ 036.js
@@ -6,3 +6,23 @@
 
 // (Please note that the palindromic number, in either base, may not include 
 // leading zeros.)
+
+function isPalindromic(a) {
+	var len = a.length;
+	if (len === 0 || len === 1) return true;
+	if (len === 2) return a[0] === a[1];
+	if (a[0] !== a[len-1]) return false;
+	return isPalindromic(a.splice(1, len-2));
+}
+
+function findPalindromicNumbersSum(max) {
+	var sum = 0;
+	while (max--) {
+		if (!isPalindromic( (max+'').split('') )) continue;
+		if (!isPalindromic( max.toString(2).split('') )) continue;
+		sum += max;
+	}
+	return sum;
+}
+
+console.log( findPalindromicNumbersSum(1000000) );
